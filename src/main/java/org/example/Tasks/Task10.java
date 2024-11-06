b118e97fd95f0a45bac24f850308fc6858571979
Gelani Aushev <aushevgelani006@gmail.com>
2024-11-06 20:57:29 +0200

    Changed Martins code

--- src/main/java/org/example/Tasks/Task10.java
+++ src/main/java/org/example/Tasks/Task10.java
@@ -1,22 +1,49 @@
 package org.example.Tasks;
 
+import java.util.Arrays;
+
 public class Task10 {
     public static void main(String[] args) {
 
         //Martin Schwarz
 
-        String[] elements = {"Car", "Bread", "Phone", "Car", "Dishwasher", "Bread"};
+        String[] fruits = {"apple", "banana", "orange", "apple", "kiwi",
+                "banana", "grape", "apple", "peach", "banana", "cherry",
+                "orange", "pear", "grape", "apple", "mango"};
 
-        System.out.println("Duplicate elements:");
+        System.out.println("Array of strings:");
 
-        for (int i = 0; i < elements.length; i++) {
-            for (int j = i + 1; j < elements.length; j++) {
-                if (elements[i].equals(elements[j])) {
-                    System.out.println(elements[i]);
-                    break;
+        System.out.println(Arrays.toString(fruits));
+        System.out.println();
+        byte[] freq = new byte[fruits.length];
+        int left = fruits.length;
+        boolean found = false;
+
+        for (int i = 0; (i < fruits.length - 1) && (left > 0); i++) {
+            if (freq[i] < 0) continue;
+            for (int j = i + 1; j < fruits.length; j++) {
+                if (fruits[i].equals(fruits[j])) {
+                    if (freq[i] >= 0) {
+                        freq[i]++;
+                        freq[j] = -1;
+                        found = true;
+                        left--;
+                    }
                 }
             }
+            left--;
         }
+        int i = 0;
+        if (found) {
+            System.out.println("Duplicate string(s):");
+            for (byte e : freq) {
+                if (e > 0)
+                    System.out.print(fruits[i] + " ");
+                i++;
+            }
+            System.out.println();
+        } else
+            System.out.println("No duplicates.");
 
     }
 }
--- src/main/java/org/example/Tasks/Task9.java
+++ src/main/java/org/example/Tasks/Task9.java
@@ -1,26 +1,31 @@
 package org.example.Tasks;
 
+import java.util.Arrays;
+
 public class Task9 {
     public static void main(String[] args) {
 
         //Martin Schwarz
 
-        int[] nums={1,2,3,4,5,6,7,8,9,10};
+        int[] arr = {-176, 37, 113, 243, 96, 249, 204, -259, -165, 257, 184, -198, 241,
+                115, 30, 156, 243, -286, 159, 45, 289, -242, 248, 267, 144, 130,
+                262, 5, 243, 67, 202, 228, 144, 124, 289, 0, 171, 60, 169, 288};
 
-        int firstLargestNum=nums[0];
-        int secondLargestNum=nums[0];
+        int premax, max;
+        premax = max = arr[0];
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > firstLargestNum) {
-                firstLargestNum=nums[i];
-            }
-            if (nums[i] > secondLargestNum && nums[i] != 10) {
-                secondLargestNum=nums[i];
-            }
+        System.out.println("Array of integers:");
+        System.out.println(Arrays.toString(arr));
+        System.out.println();
+
+        for (int e : arr) {
+            if (e > max)
+                max = e;
+            if (e > premax && e < max)
+                premax = e;
+
         }
-
-        System.out.println("First largest num is: "+firstLargestNum);
-        System.out.println("Second largest num is: "+secondLargestNum);
+        System.out.println("The second largest number in the array is " + premax + ".");
 
     }
 }
